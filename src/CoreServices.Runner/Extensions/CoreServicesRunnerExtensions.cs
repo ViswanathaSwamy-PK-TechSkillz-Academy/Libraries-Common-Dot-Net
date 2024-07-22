67e36bc40f8cda626470b95eb75371e444774690
Viswanatha Swamy <vishipayyallore@yahoo.com>
2024-07-22 17:23:24 +0530

    INitial version

---
+++ src/CoreServices.Runner/Extensions/CoreServicesRunnerExtensions.cs
@@ -0,0 +1,19 @@
+﻿using CoreServices.Runner.Interfaces;
+using HeaderFooter.Extensions;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace CoreServices.Runner.Extensions;
+
+public static class CoreServicesRunnerExtensions
+{
+
+    public static IServiceCollection ConfigureRunnerServices(this IServiceCollection services)
+    {
+        _ = services.ConfigureHeaderFooterServices();
+
+        _ = services.AddTransient<IRunnableManager, RunnableManager>();
+
+        return services;
+    }
+
+}
---
+++ src/NamesGenerator/Interfaces/IGenerateNames.cs
@@ -0,0 +1,8 @@
+﻿namespace NamesGenerator.Interfaces;
+
+public interface IGenerateNames
+{
+    string[] GetNames(int nameLength = 10, int numberOfNames = 10);
+
+    //ArrayList GetNamesArrayList(int nameLength = 10, int numberOfNames = 10);
+}
